29c7572ff1cb0eb739191590e2833534cb363ea2
7R0N1X <techsoftec.tronix@gmail.com>
2023-12-27 15:58:14 -0500

    REFACTOR : Refactorización y mejoras

--- creacion-dataset/rotador-imagenes.py
+++ creacion-dataset/rotador-imagenes.py
@@ -3,112 +3,43 @@ import os
 
 
 class RotadorImagenes:
+
     def __init__(self, directorio_destino):
-        """
-        :param directorio_destino: Directorio donde se guardarán las imágenes rotadas.
-        """
-        self.directorio_destino = directorio_destino
-        self.contador = 0
+        self.directorio_destino = directorio_destino  # Directorio donde se guardarán las imágenes rotadas
+        self.contador = 0  # Contador para nombres de archivos únicos
 
     def crear_directorio_destino(self):
-        """
-        Crea el directorio de destino si no existe.
-        """
-        if not os.path.exists(self.directorio_destino):
-            os.makedirs(self.directorio_destino)
+        if not os.path.exists(self.directorio_destino):  # Verifica si el directorio de destino existe
+            os.makedirs(self.directorio_destino)  # Crea el directorio si no existe
 
     def guardar_imagen(self, imagen, nombre_archivo):
-        """
-        :param imagen: Imagen a guardar.
-        :param nombre_archivo: Nombre del archivo para la imagen.
-        """
-        directorio_rotadas = os.path.join(self.directorio_destino)
+        directorio_rotadas = os.path.join(self.directorio_destino)  # Directorio para las imágenes rotadas
+        # Guarda la imagen en el directorio con el nombre proporcionado y calidad JPEG al 100%
         cv2.imwrite(os.path.join(directorio_rotadas, nombre_archivo), imagen, [cv2.IMWRITE_JPEG_QUALITY, 100])
 
     @staticmethod
-    def recortar_cara(imagen):
-        """
-        Recorta la región de la cara en la imagen en 48x48 píxeles.
-        :param imagen: Imagen de entrada.
-        """
-        # Carga el clasificador Haar para detección de rostros
-        cascade_clasificador = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-
-        # Convierte la imagen de color BGR a escala de grises
-        imagen_gris = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
-
-        """
-        Utiliza el clasificador Haar para detectar caras en la imagen en escala de grises
-        detectMultiScale devuelve las coordenadas (x, y, ancho, alto) de las caras detectadas
-        scaleFactor controla la compensación de escala, minNeighbors establece la detección mínima de vecinos y minSize
-        define el tamaño mínimo de la cara
-        """
-        caras = cascade_clasificador.detectMultiScale(imagen_gris, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-
-        for (x, y, w, h) in caras:
-            cara_recortada = imagen_gris[y:y+h, x:x+w]  # Región de la cara en escala de grises
-            cara_recortada_48x48 = cv2.resize(cara_recortada, (48, 48))  # Redimensionar la cara a 48x48
-            return cara_recortada_48x48
-
-        return None  # Si no se detecta ninguna cara en la imagen
-
-    def rotar_imagen(self, cara_recortada, angulo_rotacion, tipo_rotacion):
-        """
-        Aplica rotación a la imagen de la cara recortada.
-        :param cara_recortada: Imagen de la cara recortada.
-        :param angulo_rotacion: Ángulo de rotación.
-        :param tipo_rotacion: Tipo de rotación 'rotada, sin_rotar'.
-        :return:
-        """
-        if cara_recortada is not None:
-            # Obtiene la matriz de rotación para la cara recortada
-            matriz_rotacion = cv2.getRotationMatrix2D((cara_recortada.shape[1] / 2, cara_recortada.shape[0] / 2),
-                                                      angulo_rotacion, 1)
-            # Aplica la transformación de rotación a la cara recortada
-            cara_rotada = cv2.warpAffine(cara_recortada, matriz_rotacion,
-                                         (cara_recortada.shape[1], cara_recortada.shape[0]),
-                                         borderMode=cv2.BORDER_REFLECT)
-
-            # Guarda la imagen de la cara rotada con un nombre de archivo único y aumenta el contador
-            self.guardar_imagen(cara_rotada, f"{self.contador}_rotada_{tipo_rotacion}_{angulo_rotacion}.jpg")
-            self.contador += 1
+    def rotar_imagen(imagen, angulo_rotacion):
+        # Obtiene la matriz de rotación para rotar la imagen según el ángulo dado
+        matriz_rotacion = cv2.getRotationMatrix2D((imagen.shape[1] / 2, imagen.shape[0] / 2), angulo_rotacion, 1)
+        # Aplica la transformación de rotación a la imagen
+        imagen_rotada = cv2.warpAffine(imagen, matriz_rotacion, (imagen.shape[1], imagen.shape[0]),
+                                       borderMode=cv2.BORDER_REFLECT)
+        return imagen_rotada
 
     def aplicar_rotacion_carpeta(self, ruta_carpeta):
-        """
-        Aplica rotación a las imágenes en una carpeta.
-        :param ruta_carpeta: Ruta de la carpeta con las imágenes.
-        """
-        # Obtiene la lista de archivos
-        archivos = os.listdir(ruta_carpeta)
+        archivos = os.listdir(ruta_carpeta)  # Lista los archivos en la carpeta proporcionada
 
-        # Itera sobre cada archivo en la lista
         for archivo in archivos:
-
-            # Verifica si el archivo es una imagen (formatos .jpg o .png)
-            if archivo.endswith(".jpg") or archivo.endswith(".png"):
-
-                # Obtiene la ruta completa de la imagen
-                ruta_imagen = os.path.join(ruta_carpeta, archivo)
-
-                # Lee la imagen
-                imagen_original = cv2.imread(ruta_imagen)
-
-                # Recorta la cara en la imagen original
-                cara_recortada_48x48 = self.recortar_cara(imagen_original)
-
-                # Verifica si se detectó correctamente una cara en la imagen
-                if cara_recortada_48x48 is not None:
-                    # Guarda la imagen de la cara recortada
-                    self.guardar_imagen(cara_recortada_48x48, f"{self.contador}_cara_48x48.jpg")
-                    self.contador += 1
-
-                    # Lista de ángulos de rotación para la cara recortada
-                    angulos_rotacion = [30, -30]
-
-                    # Itera sobre los ángulos de rotación para aplicar la rotación
-                    for tipo_rotacion, angulo_rotacion in enumerate(angulos_rotacion):
-                        # Aplica la rotación a la cara recortada en cada ángulo
-                        self.rotar_imagen(cara_recortada_48x48, angulo_rotacion, tipo_rotacion)
+            if archivo.endswith(".jpg") or archivo.endswith(".png"):  # Verifica si el archivo es una imagen
+                ruta_imagen = os.path.join(ruta_carpeta, archivo)  # Ruta completa del archivo de imagen
+                imagen_original = cv2.imread(ruta_imagen)  # Lee la imagen original
+                self.guardar_imagen(imagen_original, f"{self.contador}_original.jpg")  # Guarda la imagen original
+                self.contador += 1  # Incrementa el contador para generar nombres únicos
+                for angulo_rotacion in [30, -30]:  # Itera sobre los ángulos de rotación
+                    imagen_rotada = self.rotar_imagen(imagen_original, angulo_rotacion)  # Rota la imagen
+                    # Guarda la imagen rotada con un nombre único
+                    self.guardar_imagen(imagen_rotada, f"{self.contador}_rotada_{angulo_rotacion}.jpg")
+                    self.contador += 1  # Incrementa el contador para generar nombres únicos
 
         cv2.destroyAllWindows()
 
@@ -116,15 +47,7 @@ class RotadorImagenes:
 if __name__ == "__main__":
     # Directorio donde se guardarán las imágenes rotadas
     directorio_destino = "E:\\Reconocimiento-Emociones-Faciales\\creacion-dataset\\caras-rotadas"
-
-    # Inicializa el objeto RotadorImagenes con el directorio de destino
-    rotador = RotadorImagenes(directorio_destino)
-
-    # Crea el directorio de destino si no existe
-    rotador.crear_directorio_destino()
-
-    # Ruta de la carpeta que contiene las imágenes originales
-    ruta_carpeta = "E:\\img"
-
-    # Aplica la rotación a las imágenes en la carpeta especificada
-    rotador.aplicar_rotacion_carpeta(ruta_carpeta)
+    rotador = RotadorImagenes(directorio_destino)  # Inicializa un objeto RotadorImagenes
+    rotador.crear_directorio_destino()  # Crea el directorio de destino si no existe
+    ruta_carpeta = "E:\\img"  # Ruta de la carpeta que contiene las imágenes originales
+    rotador.aplicar_rotacion_carpeta(ruta_carpeta)  # Aplica la rotación a las imágenes en la carpeta
