ea909e582fc82aca70e7face66206c91b7043a95
7R0N1X <techsoftec.tronix@gmail.com>
2024-01-07 07:42:48 -0500

    REFACTOR : Refactorización y mejoras

--- creacion-dataset/rotador-imagenes.py
+++ creacion-dataset/rotador-imagenes.py
@@ -2,52 +2,59 @@ import cv2
 import os
 
 
-class RotadorImagenes:
-
-    def __init__(self, directorio_destino):
-        self.directorio_destino = directorio_destino  # Directorio donde se guardarán las imágenes rotadas
-        self.contador = 0  # Contador para nombres de archivos únicos
-
-    def crear_directorio_destino(self):
-        if not os.path.exists(self.directorio_destino):  # Verifica si el directorio de destino existe
-            os.makedirs(self.directorio_destino)  # Crea el directorio si no existe
-
-    def guardar_imagen(self, imagen, nombre_archivo):
-        directorio_rotadas = os.path.join(self.directorio_destino)  # Directorio para las imágenes rotadas
-        # Guarda la imagen en el directorio con el nombre proporcionado y calidad JPEG al 100%
-        cv2.imwrite(os.path.join(directorio_rotadas, nombre_archivo), imagen, [cv2.IMWRITE_JPEG_QUALITY, 100])
-
-    @staticmethod
-    def rotar_imagen(imagen, angulo_rotacion):
-        # Obtiene la matriz de rotación para rotar la imagen según el ángulo dado
-        matriz_rotacion = cv2.getRotationMatrix2D((imagen.shape[1] / 2, imagen.shape[0] / 2), angulo_rotacion, 1)
-        # Aplica la transformación de rotación a la imagen
-        imagen_rotada = cv2.warpAffine(imagen, matriz_rotacion, (imagen.shape[1], imagen.shape[0]),
-                                       borderMode=cv2.BORDER_REFLECT)
-        return imagen_rotada
-
-    def aplicar_rotacion_carpeta(self, ruta_carpeta):
-        archivos = os.listdir(ruta_carpeta)  # Lista los archivos en la carpeta proporcionada
-
-        for archivo in archivos:
-            if archivo.endswith(".jpg") or archivo.endswith(".png"):  # Verifica si el archivo es una imagen
-                ruta_imagen = os.path.join(ruta_carpeta, archivo)  # Ruta completa del archivo de imagen
-                imagen_original = cv2.imread(ruta_imagen)  # Lee la imagen original
-                self.guardar_imagen(imagen_original, f"{self.contador}_original.jpg")  # Guarda la imagen original
-                self.contador += 1  # Incrementa el contador para generar nombres únicos
-                for angulo_rotacion in [30, -30]:  # Itera sobre los ángulos de rotación
-                    imagen_rotada = self.rotar_imagen(imagen_original, angulo_rotacion)  # Rota la imagen
-                    # Guarda la imagen rotada con un nombre único
-                    self.guardar_imagen(imagen_rotada, f"{self.contador}_rotada_{angulo_rotacion}.jpg")
-                    self.contador += 1  # Incrementa el contador para generar nombres únicos
-
-        cv2.destroyAllWindows()
+def crear_directorio_salida(ruta_salida):
+    """
+    :param ruta_salida: La ruta al directorio de salida que se creará si no existe.
+    """
+    if not os.path.exists(ruta_salida):
+        os.makedirs(ruta_salida)
 
 
-if __name__ == "__main__":
-    # Directorio donde se guardarán las imágenes rotadas
-    directorio_destino = "E:\\Reconocimiento-Emociones-Faciales\\creacion-dataset\\caras-rotadas"
-    rotador = RotadorImagenes(directorio_destino)  # Inicializa un objeto RotadorImagenes
-    rotador.crear_directorio_destino()  # Crea el directorio de destino si no existe
-    ruta_carpeta = "E:\\img"  # Ruta de la carpeta que contiene las imágenes originales
-    rotador.aplicar_rotacion_carpeta(ruta_carpeta)  # Aplica la rotación a las imágenes en la carpeta
+def guardar_imagen(imagen, nombre_archivo, ruta_salida):
+    """
+    :param imagen: La imagen a guardar.
+    :param nombre_archivo: El nombre del archivo de imagen.
+    :param ruta_salida: La ruta al directorio de salida donde se guardará la imagen.
+    """
+    cv2.imwrite(os.path.join(ruta_salida, nombre_archivo), imagen, [cv2.IMWRITE_JPEG_QUALITY, 100])
+
+
+def rotar_imagen(imagen, angulo_rotacion):
+    """
+    :param imagen: La imagen a rotar.
+    :param angulo_rotacion: El ángulo de rotación.
+    :return: La imagen rotada.
+    """
+    altura, ancho = imagen.shape[:2]
+    matriz_rotacion = cv2.getRotationMatrix2D((ancho / 2, altura / 2), angulo_rotacion, 1)
+    imagen_rotada = cv2.warpAffine(imagen, matriz_rotacion, (ancho, altura), borderMode=cv2.BORDER_REFLECT)
+    return imagen_rotada
+
+
+def aplicar_rotacion_carpeta(ruta_carpeta_entrada, ruta_carpeta_salida):
+    """
+    :param ruta_carpeta_entrada: La ruta a la carpeta que contiene las imágenes originales.
+    :param ruta_carpeta_salida: La ruta a la carpeta donde se guardarán las imágenes rotadas.
+    """
+    contador = 0
+    crear_directorio_salida(ruta_carpeta_salida)
+
+    for archivo in os.listdir(ruta_carpeta_entrada):
+        if archivo.lower().endswith((".jpg", ".png")):
+            ruta_imagen = os.path.join(ruta_carpeta_entrada, archivo)
+            imagen_original = cv2.imread(ruta_imagen)
+            if imagen_original is not None:
+                guardar_imagen(imagen_original, f"{contador}_original.jpg", ruta_carpeta_salida)
+                contador += 1
+
+                for angulo in [30, -30]:
+                    imagen_rotada = rotar_imagen(imagen_original, angulo)
+                    guardar_imagen(imagen_rotada, f"{contador}_rotada_{angulo}.jpg", ruta_carpeta_salida)
+                    contador += 1
+
+    cv2.destroyAllWindows()
+
+
+ruta_salida = 'caras-rotadas'
+ruta_entrada = 'E:\\img'
+aplicar_rotacion_carpeta(ruta_entrada, ruta_salida)
